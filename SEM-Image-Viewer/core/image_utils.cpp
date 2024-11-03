1c3875aac2d4ca0fae518fd13981f17e6ec22f35
Muhab <swe.muhabfawzy@gmail.com>
2024-11-03 18:55:08 +0200

    - Added denoising filter

--- SEM-Image-Viewer/core/image_utils.cpp
+++ SEM-Image-Viewer/core/image_utils.cpp
@@ -42,3 +42,26 @@ cv::Mat ImageUtils::diffTwoImages(const cv::Mat &image1, const cv::Mat &image2,
   cv::bitwise_and(image1, image1, res, mask);
   return res;
 }
+
+bool ImageUtils::isGreyScale(const cv::Mat &image) {
+    if (image.empty())
+        return false;
+
+    cv::Vec3b color;
+    cv::Point pt;
+
+    for (int i = 0; i < image.cols; ++i)
+    {
+        for (int b = 0; b < image.rows; ++b)
+        {
+            pt.x = i;
+            pt.y = b;
+            color = image.at<cv::Vec3b>(pt);
+
+            // Gray scale: R == G == B
+            if ((color[0] & color[1] & color[2]) != color[0])
+                return false;
+        }
+    }
+    return true;
+}
