582a9da1049744cd42226275173cacbf1cbc7205
BigFish2086 <a8686.ibrahim@gmail.com>
2024-11-07 17:49:56 +0200

    implement the logger itself and its logging function

--- SEM-Image-Viewer/core/engines/Logger.cpp
+++ SEM-Image-Viewer/core/engines/Logger.cpp
@@ -1,7 +1,23 @@
 #include "Logger.h"
 
-Logger Logger::_instance;
+std::unique_ptr<Logger> Logger::m_instance = nullptr;
+std::mutex Logger::m_mutex;
 
-Logger& Logger::Instance(void) {
-	return _instance;
+std::unique_ptr<Logger> &Logger::instance() {
+  std::lock_guard<std::mutex> lock(m_mutex);
+  if (!m_instance) {
+    m_instance = std::unique_ptr<Logger>(new Logger());
+  }
+  return m_instance;
+}
+
+void Logger::setModel(std::shared_ptr<MessageDataModel> model) {
+  m_dataModel = model;
+}
+
+void Logger::log(std::unique_ptr<IMessage> msg) {
+  if (m_dataModel == nullptr) {
+    throw std::runtime_error("Logger model is not set");
+  }
+  m_dataModel->addLogEntry(std::move(msg));
 }
