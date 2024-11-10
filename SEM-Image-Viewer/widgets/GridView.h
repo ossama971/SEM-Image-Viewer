b30aa5efd940b123e5d6479a9e3e0d154c88d804
nourhanhelmy1 <nourhanhelmy01@outlook.com>
2024-11-10 17:53:25 +0200

    Added diff view signals

--- SEM-Image-Viewer/widgets/GridView.h
+++ SEM-Image-Viewer/widgets/GridView.h
@@ -10,6 +10,10 @@
 class GridView : public QWidget {
     Q_OBJECT
 
+signals:
+    void openDiffView();
+    void openDiffViewRequested(const Image& image1, const Image& image2);
+
 public:
     explicit GridView(QWidget *parent = nullptr);
 
@@ -17,6 +21,8 @@ private slots:
     void handleSelectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
     void onImageChanged(Image* newImage);
     void onScroll(int value); // New slot to handle scroll events
+    void showContextMenu(const QPoint &pos);  // Declare showContextMenu
+    void openInDiffView();  // Declare openInDiffView
 
 private:
     QListView *listView;
@@ -25,6 +31,8 @@ private:
     void setModel(ImageDataModel *model);
     void initializeGrid();
     void loadImages(const QString &path);
+    Image firstImage;
+    Image secondImage;
 };
 
 #endif // GRIDVIEW_H
--- SEM-Image-Viewer/widgets/topmiddlewidget.h
+++ SEM-Image-Viewer/widgets/topmiddlewidget.h
@@ -24,6 +24,7 @@ public:
 
 public slots:
     void setMaxMinHeight(int mn, int mx);
+    void openDiffView();
 };
 
 #endif // TOPMIDDLEWIDGET_H
