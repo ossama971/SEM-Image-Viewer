b9a61b03771af7b2a4c2ada03725c24f14dacffa
BigFish2086 <a8686.ibrahim@gmail.com>
2024-11-13 17:47:26 +0200

    add visitor and visitable interfaces
    - both should be useful for saving the session through the visitor
      pattern

---
+++ SEM-Image-Viewer/core/data/Visitor.h
@@ -0,0 +1,21 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+
+class ImageMetadata;
+class ImageState;
+class Image;
+class ImageRepository;
+class SessionData;
+
+class Visitor {
+public:
+  virtual void visit(const ImageMetadata &metadata) = 0;
+  virtual void visit(const ImageState &state) = 0;
+  virtual void visit(const Image &image) = 0;
+  virtual void visit(const ImageRepository &repo) = 0;
+  virtual void visit(const SessionData &session) = 0;
+
+  virtual ~Visitor() = default;
+};
+
+#endif // VISITOR_H
