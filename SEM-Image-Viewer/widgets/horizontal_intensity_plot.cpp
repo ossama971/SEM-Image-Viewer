1e5954d98eccd55634d70e8902a0a97311fee5c9
Osama Mohamed Badawi <osama_badawi@outlook.com>
2024-11-25 20:15:41 +0200

    horizontal Intensity Plot

---
+++ SEM-Image-Viewer/widgets/horizontal_intensity_plot.cpp
@@ -0,0 +1,32 @@
+#include "horizontal_intensity_plot.h"
+#include "../core/engines/workspace.h"
+#include <QHBoxLayout>
+#include <QPushButton>
+
+horizontal_intensity_plot::horizontal_intensity_plot(QWidget * parent)
+    :QWidget(parent),
+    label(new QLabel(this))
+{
+    QHBoxLayout *layout = new QHBoxLayout(this);
+
+    label->setText("Intensity Plot");
+    label->setStyleSheet("QLabel {background-color:transparent}");
+    QPushButton *applyButton = new QPushButton(this);
+    QPixmap pixmap(":/icons/pen-icon.svg");
+    QIcon ButtonIcon(pixmap);
+    applyButton->setIcon(ButtonIcon);
+    applyButton->setStyleSheet(
+        "QPushButton { "
+        "padding:2px"
+        "}"
+        );
+    //Add label and Button to layout
+    layout->addWidget(label);
+    layout->addWidget(applyButton);
+    layout->addStretch();
+    layout->setSpacing(5);
+
+    setLayout(layout);
+
+    connect(applyButton, &QPushButton::clicked,&Workspace::Instance()->getActiveSession(),&SessionData::toggleHorizontalPlotMode);
+}
