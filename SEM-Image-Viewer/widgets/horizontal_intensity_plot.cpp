a2dd227ee92644aabea00150da062730f6bc07b4
Osama Mohamed Badawi <osama_badawi@outlook.com>
2024-11-26 04:49:30 +0200

    scene and intensity plot bugs fix

--- SEM-Image-Viewer/widgets/horizontal_intensity_plot.cpp
+++ SEM-Image-Viewer/widgets/horizontal_intensity_plot.cpp
@@ -11,7 +11,7 @@ horizontal_intensity_plot::horizontal_intensity_plot(QWidget * parent)
 
     label->setText("Intensity Plot");
     label->setStyleSheet("QLabel {background-color:transparent}");
-    QPushButton *applyButton = new QPushButton(this);
+    applyButton = new QPushButton(this);
     QPixmap pixmap(":/icons/pen-icon.svg");
     QIcon ButtonIcon(pixmap);
     applyButton->setIcon(ButtonIcon);
@@ -29,5 +29,26 @@ horizontal_intensity_plot::horizontal_intensity_plot(QWidget * parent)
 
     setLayout(layout);
 
+
     connect(applyButton, &QPushButton::clicked,&Workspace::Instance()->getActiveSession(),&SessionData::toggleHorizontalPlotMode);
+    connect(applyButton, &QPushButton::clicked, this, &horizontal_intensity_plot::toggleStyleSheet);
+}
+
+
+void horizontal_intensity_plot::toggleStyleSheet()
+{
+    if(Workspace::Instance()->getActiveSession().horizontalIntensityPlotMode)
+    {
+        applyButton->setStyleSheet("QPushButton {"
+                                   "background-color: rgba( 55,55,55,0.5);"  // Example color for toggle
+                                   "border-radius: 4px;"
+                                   "}");
+    }
+    else {
+        applyButton->setStyleSheet("QPushButton {"
+                                   "background-color: transparent"
+                                   "border-radius: 4px;"
+                                   "}");  // Example color for toggle
+    }
+
 }
